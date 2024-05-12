121513076b35724e5e6933c1aad953224fcef5d4
Aiko <59002254+aikobuny@users.noreply.github.com>
2024-05-12 13:38:07 +0800

    new

--- dsfi.js
+++
@@ -1,8 +0,0 @@
-function scrollE(id) {
-	let e = document.getElementById(id);
-	e.scrollIntoView({ behavior: 'smooth' });
-}
-
-function goto(url) {
-	window.location.href = window.location.origin + "/" + url;
-}
